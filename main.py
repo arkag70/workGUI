240539a903969b6eb81a3372e724dcc0156120d4
Arkaprabha Ghosh <arkag70@gmail.com>
2019-05-08 18:10:43 +0530

    added an entry to select project repository

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ def readFile(filepath):
     global count
     count += 1
     f = open(filepath, mode = 'r', encoding="Latin-1")
-    file_content = f.read()
+    file_content = f.readlines()
+    file_content = [x.strip() for x in file_content]
     return file_content
 
 def getFiles(path,extensions):
     
     #list of all .c .h .cpp .hpp files in the above path
     files = []
+    print(f"Files with extensions {extensions}:\n\n")
     # r=root, d=directories, f = files
     for r, d, f in os.walk(path):
         for file in f:
@@ -26,41 +28,45 @@ def getFiles(path,extensions):
                 if ext in extensions:
             #if '.c' in file or '.cpp' in file or '.h' in file or '.hpp' in file:
                     files.append(os.path.join(r, file))
-    
-    #list to contain contents of all the files
-    file_contents_list = []
-    
+
     for file in files:
-        file_contents_list.append(readFile(file))
+        print(file)
     
     #list to contain filenames of such files which contain stock_words
-    file_names_with_stockWords = []
+    # file_names_with_stockWords = []
     
-    for i in range(len(file_contents_list)):
-        words_to_append = ""
-        for word in stock_words:
-            if word in file_contents_list[i]:
-                words_to_append = words_to_append+word+","
-        if len(words_to_append) != 0:
-            file_names_with_stockWords.append(files[i]+" ---> "+words_to_append)
+    # for i in range(len(file_contents_list)):
+    #     words_to_append = ""
+    #     for word in stock_words:
+    #         if word in file_contents_list[i]:
+    #             words_to_append = words_to_append+word+","
+    #     if len(words_to_append) != 0:
+    #         file_names_with_stockWords.append(files[i]+" ---> "+words_to_append)
     
-    if len(file_names_with_stockWords) == 0:
-        results.insert(END,"None")
+    # if len(file_names_with_stockWords) == 0:
+    #     results.insert(END,"None")
     
-    files_number.config(text = "Files read : "+str(count)+'\t')
-    files_found.config(text = 'Files with stock_words: '+str(len(file_names_with_stockWords)))
+    # files_number.config(text = "Files read : "+str(count)+'\t')
+    # files_found.config(text = 'Files with stock_words: '+str(len(file_names_with_stockWords)))
     
-    for eachfile in file_names_with_stockWords:
-        #print(eachfile)
-        results.insert(END,eachfile)
-        results.insert(END,'\n\n')
+    # for eachfile in file_names_with_stockWords:
+    #     #print(eachfile)
+    #     results.insert(END,eachfile)
+    #     results.insert(END,'\n\n')
         
 #D:\\Arka\\StateHandlerLatestDevelop\\idc5
-
+rootname = ''
 def browse():
-    dirname = filedialog.askdirectory(parent=root, initialdir="D:\\", title='Select your folder')
+    dirname = filedialog.askdirectory(parent=root, initialdir=rootname, title='Select your Component Directory')
     entry.delete(0,END)
     entry.insert(0,dirname)
+
+def browse_root():
+    global rootname
+    rootname = filedialog.askdirectory(parent=root, initialdir="D:\\", title='Select Project folder')
+    entry_root.delete(0,END)
+    entry_root.insert(0,rootname)
+    entry.insert(0,'Browser for your Component Directory')
     
 def search():
     global count
@@ -83,32 +89,47 @@ def search():
 
 root  = Tk()
 root.config(background = 'light blue')
-root.geometry("900x450")
+root.geometry("880x450")
 root.resizable(width=False, height=False)
 root.title("Software Restrictions Filter")
 
 # layouts
 upper = Frame(root,background  = 'light blue')
-upper.grid(row = 0,column = 0,padx = 40,pady = 20)
+upper.grid(row = 0,column = 0,padx = 0,pady = 10)
+
+
+
+upper2 = Frame(root,background  = 'light blue')
+upper2.grid(row = 1,column = 0,padx = 40,pady = 20)
 
 lower = Frame(root,background  = 'white')
-lower.grid(row = 1,column = 0)
+lower.grid(row = 2,column = 0)
 
 extension_layout = Frame(root,background  = 'light blue')
-extension_layout.grid(row = 2,column = 0,pady = 10)
+extension_layout.grid(row = 3,column = 0,pady = 10)
 
 down = Frame(root,background  = 'white')
-down.grid(row = 3,column = 0)
+down.grid(row = 4,column = 0)
 
 # widgets
-entry = Entry(upper,width = 80,bd = 3)
+
+entry_root = Entry(upper,width = 80,bd = 3)
+entry_root.grid(row = 0,column = 0)
+entry_root.config(font=("Times New Roman", 12))
+entry_root.insert(0,'Browser for the Project Directory')
+
+button_root = Button(upper,text = "...",command = browse_root,width = 4,bd = 3,font=("Times New Roman", 10))
+button_root.grid(row = 0,column = 1)
+
+
+entry = Entry(upper2,width = 80,bd = 3)
 entry.grid(row = 0,column = 0)
 entry.config(font=("Times New Roman", 12))
 
-button_browse = Button(upper,text = "browse",command = browse,width = 10,bd = 3,font=("Times New Roman", 10))
-button_browse.grid(row = 0,column = 1,padx = 10)
+button_browse = Button(upper2,text = "...",command = browse,width = 4,bd = 3,font=("Times New Roman", 10))
+button_browse.grid(row = 0,column = 1)
 
-button_search = Button(upper,text = "search",command = search,width = 10,bd = 3,font=("Times New Roman", 10))
+button_search = Button(upper2,text = "search",command = search,width = 10,bd = 3,font=("Times New Roman", 10))
 button_search.grid(row = 0,column = 2,padx = 5)
 
 v_scrollbar = Scrollbar(lower,orient = VERTICAL,bd = 2) 
